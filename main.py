a3722e8a25516ba298bd66c4218a343faee28e32
SeulKing-Ja <jarudet.king@gmail.com>
2025-08-17 03:29:14 +0700

    specific amount of data

--- main.py
+++ main.py
@@ -20,47 +20,79 @@ filter_ul = txt.find_all(['ul'], id='menu-menu')
 
 links = []
 for filter in filter_ul:
-    f = filter.find_all(['li'], class_=re.compile('^menu-*'))
+    f = filter.find_all(['li'], class_=re.compile(r'menu-\D+'))
     for link in f:
         add_link = link.find('a')
         links.append(add_link.get('href'))
+links.pop(0)
+
+page_count = 1 
+max_page = 2
+post_count = 1
+max_post = 200
 
 
-url = links[1]
-web = requests.get(url, headers=headers)
-html = BeautifulSoup(web.text, 'html.parser')
+post_to_csv = []
+post_seen = set()
 
-cp_post = html.find_all('div', id=re.compile(r'^post-\d+'))
+for link_web in links:
+    url = link_web
+    while True:
+        web = requests.get(url, headers=headers)
+        html = BeautifulSoup(web.text, 'html.parser')
+        cp_post = html.find_all('div', id=re.compile(r'^post-\d+'))
 
-post_data = []
-for post in cp_post:
-    post_img = post.find('img', class_='attachment-140x140')
-    img_text = post_img.get('src') if post_img else " "
+        if not cp_post:
+            break
 
-    post_title = post.find('h2', class_="title")
-    title_text = post_title.get_text(strip=True) if post_title else ' '
+        for post in cp_post: 
+            if post_count == max_post:
+                break
+            post_title = post.find('h2', class_="title")
+            title_text = post_title.get_text(strip=True) if post_title else ' '
 
-    post_date = post.find('div', class_='post-date')
-    date_text = post_date.get_text(strip=True) if post_date else ' '
+            #check duplicate
+            if title_text in post_seen:
+                continue
+            post_seen.add(title_text)
 
-    post_info = post.find('div', class_='post-info')
-    if post_info:
-        cate = post_info.find_all('a', attrs={'title': True})
-        cate_text = ', '.join(i.get_text(strip=True)for i in cate)
-    else:
-        cate_text = ' '
+            post_img = post.find('img', class_='attachment-140x140')
+            img_text = post_img.get('src') if post_img else " "
 
-    post_content = post.find('div', class_='post-content')
-    content_text = post_content.get_text(strip=True) if post_content else ' '
-    
-    post_data.append({
-        "Title": title_text,
-        "Img": img_text,
-        "Date": date_text,
-        "Category": cate_text,
-        "Content" : content_text
-    })
+            post_date = post.find('div', class_='post-date')
+            date_text = post_date.get_text(strip=True) if post_date else ' '
 
-df = pd.DataFrame(post_data)
-df.to_csv("posts.csv", index=False, encoding="utf-8-sig")
+            post_info = post.find('div', class_='post-info')
+            if post_info:
+                cate = post_info.find_all('a', attrs={'title': True})
+                cate_text = ', '.join(i.get_text(strip=True)for i in cate)
+            else:
+                cate_text = ' '
+
+            post_content = post.find('div', class_='post-content')
+            content_text = post_content.get_text(strip=True) if post_content else ' '
+            
+            post_to_csv.append({
+                "Title": title_text,
+                "Img": img_text,
+                "Date": date_text,
+                "Category": cate_text,
+                "Content" : content_text
+            })
+            post_count += 1 
+
+        if page_count >= max_page:
+            page_count = 0
+            break
+        
+        #next
+        next = html.find('a', class_='next')
+        if next and next.get('href'):
+            url = next['href']
+            page_count += 1
+        else:
+            break
+
+df = pd.DataFrame(post_to_csv)
+df.to_csv("data.csv", index=False, encoding="utf-8-sig")
 print(f"saved")
